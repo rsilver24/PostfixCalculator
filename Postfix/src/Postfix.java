2d5f2e403bbf7b973aa68576f9bc6c4a60391360
rsilver24 <rsilver24@kentdenver.org>
2023-11-02 12:32:46 -0600

    weird update for trials

--- Postfix/src/Postfix.java
+++ Postfix/src/Postfix.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Postfix {
-    public static void main(String[] args) {
-        
+    public int lookAtThisCoolCode(){
+        Scanner s = new Scanner();
+        System.out.print("Enter expression in postfix notation w/ spaces: ");
+        String userInput = s.nextLine();
+        int length = userInput.length();
+
+        // Find number of multiplication and division precedures
+
+        int numMultDiv = 0;
+        for (int i = 0; i < length; i++){
+            if (userInput.charAt(i) == '*'){
+                numMultDiv++;
+            }
+        }
+
+        // Multiply and Divide Items Together
+
+        for (int i = numMultDiv; i != 0; i--){
+            boolean finish = false;
+            int placeHolder;
+            for (int k = 0; !finish; k++){
+                if (userInput.charAt(i) == '*'){
+                }
+            }
+        }
     }
 }
