99bf1199a955dafaf2c6714279906b896b0d7ecc
rsilver24 <rsilver24@kentdenver.org>
2023-11-08 13:52:15 -0700

    Version updated for txt files

--- Postfix/src/PostfixTestDriver.java
+++ Postfix/src/PostfixTestDriver.java
@@ -1,12 +1,30 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+// Roan Silver      Created: November 6th, 2023        Last Edits: November 6th, 2023
+// Collaborators:
 public class PostfixTestDriver {
     public static void main(String[] args) {
         Postfix p = new Postfix();
 
-        System.out.println("Given the Equation: 8 * 4 - 2 - 4 / 2 * 6 * 2 - 4 - 4 - 4");
-        System.out.println("Postfix becomes: 8 4 * 2 - 4 2 / 6 * 2 * - 4 - 4 - 4 -");
-        System.out.println("When put into the calculator, the answer should be -6");
-        System.out.println("Input '8 4 * 2 - 4 2 / 6 * 2 * - 4 - 4 - 4 -' to try it out!");
-        System.out.println(" ");
-        p.postfixCalculation();
+        System.out.print("Input the name of the file you wish to calculate: ");
+
+        try {
+            Scanner s = new Scanner(System.in);
+            File fileInput = new File( s.nextLine() + ".txt");
+            Scanner sc = new Scanner(fileInput);
+            String problem = sc.nextLine();
+            System.out.println(problem);
+            int conclusion = p.postfixCalculation(problem);
+            File fileOutput = new File("Answer");
+            FileWriter answer = new FileWriter("Answer");
+            answer.write(conclusion);
+            answer.close();
+            System.out.println("New File: " + fileOutput.getName() + " has been created!");
+        } catch (IOException e){
+            System.out.println("idk an error happened or smthn");
+            e.printStackTrace();
+        }
     }
 }
